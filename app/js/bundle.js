d18409c1d2fecb579637ce26a4d4a1ab623a81fb
Eduard Cot <ecot@itec.cat>
2016-11-08 14:56:51 +0100

    initial

--- app/js/bundle.js
+++ app/js/bundle.js
@@ -51,12 +51,17 @@
 /* 1 */
 /***/ function(module, exports) {
 
-	'use strict';
+	"use strict";
 
-	function processParagraph(text) {
+	function processParagraph(paragraph) {
+	  var header = 0;
+	  while (paragraph.charAt(0) == "%") {
+	    paragraph = paragraph.slice(1);
+	    header++;
+	  }
 
-	  return { content: '',
-	    type: '' };
+	  return { type: header == 0 ? "p" : "h" + header,
+	    content: paragraph };
 	}
 
 	console.log(processParagraph("<p>hola que tal estem</p>"));
--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,13 @@
 
+function processParagraph(paragraph) {
+  var header = 0;
+  while (paragraph.charAt(0) == "%") {
+    paragraph = paragraph.slice(1);
+    header++;
+  }
 
-
-
-
-function processParagraph(text){
-
-
-
-
-  return {  content: '',
-            type: '' }
+  return {type: (header == 0 ? "p" : "h" + header),
+          content: paragraph};
 }
 
 console.log(processParagraph("<p>hola que tal estem</p>"));
